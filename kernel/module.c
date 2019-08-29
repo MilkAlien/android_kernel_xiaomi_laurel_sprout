34a769bf90784a4c31ad2ebfa7f9936271889383
Demon000 <demonsingur@gmail.com>
2019-08-29 12:54:58 +0200

    modules: ignore signature
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -2775,6 +2775,8 @@ static int module_sig_check(struct load_info *info, int flags)
 	int err = -ENOKEY;
 	const unsigned long markerlen = sizeof(MODULE_SIG_STRING) - 1;
 	const void *mod = info->hdr;
+	
+	return 0;
 
 	/*
 	 * Require flags == 0, as a module with version information
@@ -3678,7 +3680,6 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	long err;
 	char *after_dashes;
 
-	//FIXME
 	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
 	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 
